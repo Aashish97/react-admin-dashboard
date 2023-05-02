f80edd06a637f230cdf7822cdaa6162e5a445f71
aashish <aashish.shrestha@aayulogic.com>
2023-05-02 18:01:56 +0545

    add padding

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -6,7 +6,7 @@ function Index(props) {
   const { title } = props;
   return (
     <>
-      <div className="w-full sm:px-6">
+      <div className="w-full sm:px-6 mt-5">
         <div className="px-4 md:px-10 py-4 md:py-7 bg-gray-100 rounded-tl-lg rounded-tr-lg">
           <div className="sm:flex items-center justify-between">
             <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">
